2e25c53fcd7603417da8f884480ffd3072784c31
I539173 <rohit.ravichandran@sap.com>
2024-06-25 17:17:06 +0530

    cors

--- be/server.js
+++ be/server.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: /\.vercel\.app$/,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+}));
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
